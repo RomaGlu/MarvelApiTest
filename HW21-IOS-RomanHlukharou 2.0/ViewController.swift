4ab8147c80356724e7bc3341a851337c99d8854e
Roman Hlukharou <roma.glu.69@gmail.com>
2023-10-08 15:43:29 +0300

    Initial Commit

---
+++ HW21-IOS-RomanHlukharou 2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW21-IOS-RomanHlukharou 2.0
+//
+//  Created by Роман Глухарев on 8.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
