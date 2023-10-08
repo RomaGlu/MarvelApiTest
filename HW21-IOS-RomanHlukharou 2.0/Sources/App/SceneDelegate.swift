d73b8afa0b62f460e4f37b15175bc464c9c3d502
Roman Hlukharou <roma.glu.69@gmail.com>
2023-10-08 15:50:37 +0300

    project setup finish

---
+++ HW21-IOS-RomanHlukharou 2.0/Sources/App/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  HW21-IOS-RomanHlukharou 2.0
+//
+//  Created by Роман Глухарев on 8.10.23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
